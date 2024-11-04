ca45001c23399d2cc22cd8c36aece8cf1f0a00e1
Adam Olanrewaju <olanrewajuadam2020@gmail.com>
2024-11-04 17:14:42 +0100

    first push

--- kwise_api/settings/production.py
+++ kwise_api/settings/production.py
@@ -1,6 +1,49 @@
+from .base import *
+from dotenv import load_dotenv
+load_dotenv()
+DEBUG = os.getenv('DEBUG')
 
-# CORS_ALLOWED_ORIGINS = [
-#     "",
-# ]
 
-# CSRF_TRUSTED_ORIGINS = ['']
+DATABASES = {
+  'default': {
+    'ENGINE': 'django.db.backends.postgresql',
+    'NAME': os.getenv('DB_NAME'),
+    'USER': os.getenv('DB_USER'),
+    'PASSWORD': os.getenv('DB_PASSWORD'),
+    'HOST': os.getenv('DB_HOST'),
+    'PORT': '',
+  }
+}
+
+
+CACHES = {
+  'default': {
+    'BACKEND': 'django.core.cache.backends.db.DatabaseCache',
+    'LOCATION': 'kwise_cache_table',
+    },
+  'password_reset': {
+    'BACKEND': 'django.core.cache.backends.db.DatabaseCache',
+    'LOCATION': 'password_reset_cache_table',
+  },
+  'email_verification': {
+      'BACKEND': 'django.core.cache.backends.db.DatabaseCache',
+      'LOCATION': 'email_verification_cache_table',
+    },
+}
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_PORT = os.getenv('EMAIL_PORT')
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS')
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+
+
+# payment
+PAYSTACK_SECRET_KEY = os.getenv('PAYSTACK_SECRET_KEY')
+PAYSTACK_PUBLIC_KEY = os.getenv('PAYSTACK_PUBLIC_KEY')
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
